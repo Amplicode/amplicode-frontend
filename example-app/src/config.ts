1a4d41b8f6c875b2092acd8ab847481c2d543cfe
Vyacheslav Pushkin <v.pushkin@haulmont.com>
2022-02-01 17:26:13 +0400

    feat: implement initialization hook for React Buddy #145
    affects: @amplicode/codegen

--- example-app/src/config.ts
+++ example-app/src/config.ts
@@ -3,3 +3,5 @@ export const LOGIN_URI = process.env.REACT_APP_LOGIN_URI ?? "/login";
 export const LOGOUT_URI = process.env.REACT_APP_LOGOUT_URI ?? "/logout";
 export const REQUEST_SAME_ORIGIN =
   process.env.REACT_APP_REQUEST_SAME_ORIGIN === "true";
+export const DEV_LOGIN = process.env.REACT_APP_DEV_LOGIN ?? "admin";
+export const DEV_PASSWORD = process.env.REACT_APP_DEV_PASSWORD ?? "admin";
--- packages/codegen/src/generators/react-typescript/app/template/src/config.ts
+++ packages/codegen/src/generators/react-typescript/app/template/src/config.ts
@@ -1,4 +1,6 @@
 export const GRAPHQL_URI = process.env.REACT_APP_GRAPHQL_URI ?? "/graphql";
 export const LOGIN_URI = process.env.REACT_APP_LOGIN_URI ?? "/login";
 export const LOGOUT_URI = process.env.REACT_APP_LOGOUT_URI ?? "/logout";
-export const REQUEST_SAME_ORIGIN = process.env.REACT_APP_REQUEST_SAME_ORIGIN === "true"
+export const REQUEST_SAME_ORIGIN = process.env.REACT_APP_REQUEST_SAME_ORIGIN === "true";
+export const DEV_LOGIN = process.env.REACT_APP_DEV_LOGIN ?? "admin";
+export const DEV_PASSWORD = process.env.REACT_APP_DEV_PASSWORD ?? "admin";
