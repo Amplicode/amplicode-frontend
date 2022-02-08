d47368fd3abbb3d2939490a1171d466fee885f7a
Ilya Trifonov <i.trifonov@haulmont.com>
2022-02-08 18:23:15 +0300

    chore: adding TSDoc notes for securityStore #146
    affects: @amplicode/codegen

--- example-app/src/core/security/security-store.ts
+++ example-app/src/core/security/security-store.ts
@@ -1,3 +1,7 @@
 import { SecurityStore } from "./security";
 
+/**
+ * Need to use a singleton constant because it's used outside of the React context.
+ */
+
 export const securityStore = new SecurityStore();
