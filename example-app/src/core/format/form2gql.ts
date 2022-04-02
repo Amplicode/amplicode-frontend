85c3bfcdf8017c93ff32dcc88b914d620f6f8561
Vadim Basko <vadim.p.basko@gmail.com>
2022-04-02 19:44:56 +0400

    fix: pet editor failed to submit if owner or type is not empty #267
    affects: @amplicode/codegen, @amplicode/react-core

--- example-app/src/core/format/form2gql.ts
+++ example-app/src/core/format/form2gql.ts
@@ -1,5 +1,8 @@
+import { filterFieldValues } from "@amplicode/react-core";
+
 export function form2gql(
   fieldValues: Record<string, unknown>
 ): Record<string, unknown> {
-  return fieldValues;
+  // do not send __typename to back end on form submit
+  return filterFieldValues(fieldValues, ["__typename"]);
 }
---
+++ packages/react-core/src/test/util/filterFieldValues.test.tsx
@@ -0,0 +1,111 @@
+import {filterFieldValues} from "../../util/filterFieldValues";
+import { expect } from 'chai'
+
+const fieldValues = {
+  "birthDate": "2022-06-01",
+  "identificationNumber": "1232",
+  "__typename": "PetDTO",
+  "owner": {
+    "id": 6,
+    "firstName": "Fernando",
+    "lastName": "Mitchell",
+    "__typename": "OwnerDTO"
+  },
+  "type": {
+    "id": 1,
+    "name": "DOG",
+    "__typename": "PetTypeDTO"
+  }
+};
+
+const fieldValuesWithArray = {
+  "birthDate": "2022-06-01",
+  "identificationNumber": "1232",
+  "__typename": "PetDTO",
+  "owner": [
+    {
+      "id": 6,
+      "firstName": "Fernando",
+      "lastName": "Mitchell",
+      "__typename": "OwnerDTO"
+    },
+    {
+      "id": 7,
+      "firstName": "John",
+      "lastName": "Doe",
+      "__typename": "OwnerDTO"
+    },
+  ],
+  "type": {
+    "id": 1,
+    "name": "DOG",
+    "__typename": "PetTypeDTO"
+  }
+};
+
+describe('filterFieldValues()', () => {
+  it('works with empty sortOutFields', () => {
+    let result = filterFieldValues(fieldValues, []);
+    expect(result).deep.equal(fieldValues);
+
+    result = filterFieldValues(fieldValuesWithArray, []);
+    expect(result).deep.equal(fieldValuesWithArray);
+  });
+
+  it('filter all __typename fields recursively from object', () => {
+    const result = filterFieldValues(fieldValues, ['__typename']);
+    expect(result).not.have.property('__typename');
+    expect(result.birthDate).eq("2022-06-01");
+    expect(result.identificationNumber).eq("1232");
+
+    expect(result.type).not.have.property('__typename');
+    expect(result.type).have.property('id');
+    expect(result.type).have.property('name');
+
+    expect(result.owner).not.have.property('__typename');
+    expect(result.owner).have.property('id');
+    expect(result.owner).have.property('firstName');
+    expect(result.owner).have.property('lastName');
+  });
+
+  it('filter more than one field recursively from object', () => {
+    const result = filterFieldValues(fieldValues, ['__typename', 'id']);
+    expect(result).not.have.property('__typename');
+    expect(result.birthDate).eq("2022-06-01");
+    expect(result.identificationNumber).eq("1232");
+
+    expect(result.type).not.have.property('__typename');
+    expect(result.type).not.have.property('id');
+    expect(result.type).have.property('name');
+
+    expect(result.owner).not.have.property('__typename');
+    expect(result.owner).not.have.property('id');
+    expect(result.owner).have.property('firstName');
+    expect(result.owner).have.property('lastName');
+  });
+
+  it('filter more than one field recursively from object that contain array', () => {
+    const result = filterFieldValues(fieldValuesWithArray, ['__typename', 'id']);
+    expect(result).not.have.property('__typename');
+    expect(result).not.have.property('id');
+    expect(result.birthDate).eq("2022-06-01");
+    expect(result.identificationNumber).eq("1232");
+
+    const owner0 = (result.owner as any)[0];
+    const owner1 = (result.owner as any)[1];
+
+    expect(owner0).not.have.property('id');
+    expect(owner0).not.have.property('__typename');
+    expect(owner0).have.property('firstName');
+    expect(owner0).have.property('lastName');
+
+    expect(owner1).not.have.property('id');
+    expect(owner1).not.have.property('__typename');
+    expect(owner1).have.property('firstName');
+    expect(owner1).have.property('lastName');
+
+    expect(result.type).not.have.property('id');
+    expect(result.type).not.have.property('__typename');
+    expect(result.type).have.property('name');
+  });
+});
