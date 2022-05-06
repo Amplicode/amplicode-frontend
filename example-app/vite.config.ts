a1ab3c264e5f296bcdce56965821d2b88eac3549
Ilya Trifonov <i.trifonov@haulmont.com>
2022-05-06 16:09:36 +0300

    chore: improve dev server proxy configuration #338
    affects: @amplicode/codegen

--- example-app/vite.config.ts
+++ example-app/vite.config.ts
@@ -1,14 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const PORT = "8080";
+const SERVER_URL = "http://localhost";
+
 export default defineConfig({
   assetsInclude: ["**/*.graphql"],
   plugins: [react()],
   server: {
     proxy: {
-      "/graphql": "http://localhost:8080",
-      "/login": "http://localhost:8080",
-      "/logout": "http://localhost:8080"
+      "^/(graphql|login|logout)": `${SERVER_URL}:${PORT}`
     }
   }
 });
