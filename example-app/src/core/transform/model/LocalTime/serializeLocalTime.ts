33f3ee6b806d2b9eb5274bc45d490d616cd92346
Vyacheslav Pushkin <v.pushkin@haulmont.com>
2022-05-23 18:18:51 +0400

    chore: re-generate example-app

--- example-app/src/core/transform/model/LocalTime/serializeLocalTime.ts
+++ example-app/src/core/transform/model/LocalTime/serializeLocalTime.ts
@@ -1,5 +1,5 @@
 import { Dayjs } from "dayjs";
 
 export function serializeLocalTime(value?: Dayjs) {
-  return value != null ? value.format("HH:mm:ss.SSS") : value;
+  return value != null ? value.format("HH:mm:ss") : value;
 }
