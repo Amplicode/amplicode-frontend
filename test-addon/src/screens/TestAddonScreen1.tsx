387abb54818d82a574ca9dfc96721af82299b928
Vyacheslav Pushkin <v.pushkin@haulmont.com>
2022-06-15 16:22:47 +0400

    chore: remove test-addon
    test-addon is in a separate repo
    (https://github.com/Amplicode/amplicode-default-addon)

--- test-addon/src/screens/TestAddonScreen1.tsx
+++
@@ -1,9 +0,0 @@
-import { FormattedMessage } from "react-intl";
-
-export function TestAddonScreen1() {
-  return (
-    <div>
-      <FormattedMessage id="TestAddon.TestAddonScreen1Caption" />
-    </div>
-  )
-}
