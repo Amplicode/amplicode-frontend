b61113ba28f323c68df6284f874873e061376f30
Ilya Trifonov <i.trifonov@haulmont.com>
2022-06-16 09:54:21 +0300

    feat: add multiselect table #411
    affects: @amplicode/codegen

--- packages/codegen/src/common/studio/studio-model.ts
+++ packages/codegen/src/common/studio/studio-model.ts
@@ -33,6 +33,9 @@ export interface StudioTemplateProperty {
   step?: {
     name: string;
     order: "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9";
+  };
+  showIf?: {
+    [key: string]: string | boolean | string[] | Array<string[]>;
   }
   // --not supported
   //advanced: boolean;
