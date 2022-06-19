76bbbd71936afde4f41d3bde1aa03457299a35a9
Vadim Basko <vadim.p.basko@gmail.com>
2022-06-19 13:04:57 +0400

    test: test structure improvements
    affects: @amplicode/codegen

---
+++ packages/codegen/test/integration/app/app.test.ts
@@ -0,0 +1,59 @@
+import fs from "fs";
+import yaml from "js-yaml";
+import path from "path";
+import {expect} from "chai";
+import assert from "assert";
+import {cleanup, generate, GENERATORS_DEST_DIR, GENERATORS_DIR, opts, SCHEMA2_PATH, SCHEMA_PATH} from "../common";
+
+const DEST_DIR = path.join(GENERATORS_DEST_DIR, 'app');
+const GENERATOR_DIR = path.join(GENERATORS_DIR, 'app');
+
+
+const answers = {
+  appTitle: 'Amplicode Petclinic',
+  appShortName: 'ampl-petclinic',
+  graphqlUri: '/graphql',
+  basePath: 'front',
+  menuType: 'vertical'
+};
+
+describe('codegen app test', () => {
+
+  it('should generate app (single schema file)', async () => {
+    await cleanup(DEST_DIR);
+
+    const codegenConfigPath = path.join(DEST_DIR, 'codegen.yml');
+    const devEnvPath = path.join(DEST_DIR, '.env.development');
+    const prodEnvPath = path.join(DEST_DIR, '.env.production');
+
+    // check that we remove previously generated app
+    assert.ok(!fs.existsSync(codegenConfigPath));
+    assert.ok(!fs.existsSync(devEnvPath));
+
+    await generate(GENERATOR_DIR, opts(DEST_DIR, answers, [SCHEMA_PATH]));
+
+    assert.ok(fs.existsSync(codegenConfigPath));
+    assert.ok(fs.existsSync(devEnvPath));
+    assert.ok(fs.existsSync(prodEnvPath));
+
+    const doc = yaml.load(fs.readFileSync(codegenConfigPath, 'utf-8'));
+    expect((doc as any).schema).eq('./src/core/schema/schema.graphql');
+
+    expect(fs.readFileSync(devEnvPath, 'utf-8')).to.contain('VITE_GRAPHQL_URI=/graphql');
+    expect(fs.readFileSync(prodEnvPath, 'utf-8')).to.contain('VITE_GRAPHQL_URI=/graphql');
+
+    const appSchemaPath = path.join(DEST_DIR, 'src', 'core', 'schema', 'schema.graphql');
+    expect(fs.readFileSync(appSchemaPath, 'utf-8')).to.contain('type PetDTO {');
+    expect(fs.readFileSync(appSchemaPath, 'utf-8')).not.to.contain('type TestEntity {');
+  });
+
+  it('should generate app (composite schema)', async () => {
+    const schemaPaths = [SCHEMA_PATH, SCHEMA2_PATH];
+    await generate(GENERATOR_DIR, opts(DEST_DIR, answers, schemaPaths));
+
+    const appSchemaPath = path.join(DEST_DIR, 'src', 'core', 'schema', 'schema.graphql');
+    expect(fs.readFileSync(appSchemaPath, 'utf-8')).to.contain('type PetDTO {');
+    expect(fs.readFileSync(appSchemaPath, 'utf-8')).to.contain('type TestEntity {');
+  });
+});
+
---
+++ packages/codegen/test/integration/update-schema/update-schema.test.ts
@@ -0,0 +1,31 @@
+import path from "path";
+import {cleanup, generate, GENERATORS_DEST_DIR, GENERATORS_DIR, opts, SCHEMA2_PATH, SCHEMA_PATH} from "../common";
+import {expect} from "chai";
+import fs from "fs";
+
+const GENERATOR_DIR = path.join(GENERATORS_DIR, 'update-schema');
+const DEST_DIR = path.join(GENERATORS_DEST_DIR, 'update-schema');
+
+describe('codegen update schema', async () => {
+  before(async () => { await cleanup(DEST_DIR)});
+
+  it('should update bundled schema', async () => {
+    const bundledSchemaPath = path.join(DEST_DIR, 'src', 'core', 'schema', 'schema.graphql');
+    // check that cleanup is completed, before test start
+    expect(fs.existsSync(bundledSchemaPath)).to.be.false;
+
+    let bundledSchema: string;
+
+    // Single schema file
+    await generate(GENERATOR_DIR, opts(DEST_DIR, {}, [SCHEMA_PATH]));
+    bundledSchema = fs.readFileSync(bundledSchemaPath, 'utf-8');
+    expect(bundledSchema).to.contain('type PetDTO');
+    expect(bundledSchema).to.not.contain('type TestEntity');
+
+    // Composite schema
+    await generate(GENERATOR_DIR, opts(DEST_DIR, {}, [SCHEMA_PATH, SCHEMA2_PATH]));
+    bundledSchema = fs.readFileSync(bundledSchemaPath, 'utf-8');
+    expect(bundledSchema).to.contain('type PetDTO');
+    expect(bundledSchema).to.contain('type TestEntity');
+  });
+});
