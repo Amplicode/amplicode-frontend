05e55dabf2b8dbaf9d2c4038f859907fce3fd5d1
Vadim Basko <vadim.p.basko@gmail.com>
2022-06-20 20:09:16 +0400

    test: x-to-many tests
    affects: @amplicode/codegen

--- packages/codegen/test/integration/entity-lookup/lookup-cards.test.ts
+++ packages/codegen/test/integration/entity-lookup/lookup-cards.test.ts
@@ -2,7 +2,7 @@ import fs from "fs";
 import path from "path";
 import {cleanup, generate, GENERATORS_DEST_DIR, GENERATORS_DIR, opts, SCHEMA_PATH} from "../common";
 import {expect} from "chai";
-import {ownerListQuery, scalarsListQuery} from "../queries";
+import {ownerListQuery, petListQuery, scalarsListQuery} from "../queries";
 import {expectFileContainsIgnoreSpace} from "../../../src/test/test-commons";
 import {unCapitalizeFirst} from "../../../src/common/utils";
 
@@ -85,6 +85,57 @@ describe('codegen lookup cards', () => {
 
     expectFileContainsIgnoreSpace(componentFile,
       `<ValueWithLabel key="url" label="Url" value={item.url ?? undefined} />`)
+  });
+
+
+  it('should generate lookup cards screen - Pet', async () => {
+
+    const answers = {
+      componentName: 'PetLookupCards',
+      route: 'pet-lookup-cards',
+      query: petListQuery
+    };
+    const componentPath = path.join(DEST_DIR, 'PetLookupCards.tsx');
+    // check that cleanup is completed, before test start
+    expect(fs.existsSync(componentPath)).to.be.false;
+
+    await generate(GENERATOR_DIR, opts(DEST_DIR, answers, [SCHEMA_PATH]));
+
+    const componentFile = fs.readFileSync(componentPath, 'utf-8');
+    expect(componentFile).to.contain('export function PetLookupCards(props: PetLookupCardsProps) {');
+    expect(componentFile).to.contain('query Get_Pet_List {');
+
+    expectFileContainsIgnoreSpace(componentFile,
+      `import { getTagDTODisplayName } from "core/display-name/getTagDTODisplayName";`);
+    expectFileContainsIgnoreSpace(componentFile,
+      `import { getPetDiseaseDTODisplayName } from "core/display-name/getPetDiseaseDTODisplayName";`);
+
+    expectFileContainsIgnoreSpace(componentFile,`
+      <ValueWithLabel
+        key="tags"
+        label="Tags"
+        value={
+          item.tags &&
+          item.tags
+            .map(entry => getTagDTODisplayName(entry))
+            .filter(entry => entry !== "")
+            .join(", ")
+        }
+      />
+      `);
+    expectFileContainsIgnoreSpace(componentFile,`
+      <ValueWithLabel
+        key="diseases"
+        label="Diseases"
+        value={
+          item.diseases &&
+          item.diseases
+            .map(entry => getPetDiseaseDTODisplayName(entry))
+            .filter(entry => entry !== "")
+            .join(", ")
+        }
+      />
+      `);
   });
 
 });
---
+++ packages/codegen/test/integration/entity-management/management-pet-table.test.ts
@@ -0,0 +1,79 @@
+import {cleanup, generate, GENERATORS_DEST_DIR, GENERATORS_DIR, opts, SCHEMA_PATH} from "../common";
+import {
+  petDeleteMutation,
+  petDetailsQuery,
+  petListQuery,
+  petUpsertMutation
+} from "../queries";
+import path from "path";
+import {expectFileContainsIgnoreSpace} from "../../../src/test/test-commons";
+import fs from "fs";
+
+
+const GENERATOR_DIR = path.join(GENERATORS_DIR, 'entity-management');
+const DEST_DIR = path.join(GENERATORS_DEST_DIR, 'entity-management');
+
+
+describe('codegen pet table management', () => {
+
+  beforeEach(async () => await cleanup(DEST_DIR));
+
+  it('should generate pet table management', async () => {
+
+    const answers = {
+      listComponentName: 'PetTable',
+      itemComponentName: 'PetTableEditor',
+      route: 'pet-table',
+      type: 'table',
+      shouldAddToMenu: true,
+      listQuery: petListQuery,
+      detailsQuery: petDetailsQuery,
+      deleteMutation: petDeleteMutation,
+      upsertMutation: petUpsertMutation,
+    };
+    const componentPath = path.join(DEST_DIR, 'PetTable.tsx');
+
+    await generate(GENERATOR_DIR, opts(DEST_DIR, answers, [SCHEMA_PATH]));
+
+    const componentFile = fs.readFileSync(componentPath, 'utf-8');
+
+    expectFileContainsIgnoreSpace(componentFile,
+      `import { getTagDTODisplayName } from "core/display-name/getTagDTODisplayName";`);
+    expectFileContainsIgnoreSpace(componentFile,
+      `import { getPetDiseaseDTODisplayName } from "core/display-name/getPetDiseaseDTODisplayName";`);
+
+    expectFileContainsIgnoreSpace(componentFile,`
+    {
+      title: "Diseases",
+      dataIndex: "diseases",
+      key: "diseases"
+    }
+    `);
+    expectFileContainsIgnoreSpace(componentFile,`
+    {
+      title: "Tags",
+      dataIndex: "tags",
+      key: "tags"
+    }
+    `);
+
+    expectFileContainsIgnoreSpace(componentFile,`
+    tags:
+      item &&
+      item.tags &&
+      item.tags
+        .map(entry => getTagDTODisplayName(entry))
+        .filter(entry => entry !== "")
+        .join(", ")
+    `);
+    expectFileContainsIgnoreSpace(componentFile,`
+    diseases:
+      item &&
+      item.diseases &&
+      item.diseases
+        .map(entry => getPetDiseaseDTODisplayName(entry))
+        .filter(entry => entry !== "")
+        .join(", ")
+    `);
+  });
+});
