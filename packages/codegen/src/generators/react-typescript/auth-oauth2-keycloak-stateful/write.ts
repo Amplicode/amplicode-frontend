0a46c6cdbb5274ebdd93bba48d08b0f35df474b2
Vyacheslav Pushkin <v.pushkin@haulmont.com>
2022-07-12 18:18:49 +0400

    refactor: code organization
    affects: @amplicode/codegen

--- packages/codegen/src/generators/react-typescript/auth-oauth2-keycloak-stateful/write.ts
+++ packages/codegen/src/generators/react-typescript/auth-oauth2-keycloak-stateful/write.ts
@@ -1,8 +1,8 @@
 import {YeomanGenerator} from "../../../building-blocks/YeomanGenerator";
 import {EmptyTemplateModel} from "../../../building-blocks/stages/template-model/pieces/EmptyTemplateModel";
 import {writeTemplate} from "../../../building-blocks/stages/writing/pieces/writeTemplate";
-import {replaceEnvVarValueInFile} from "../../../building-blocks/stages/writing/pieces/replace/replaceEnvVarValue";
-import {replaceStringValueInFile} from "../../../building-blocks/stages/writing/pieces/replace/replaceStringValue";
+import {replaceEnvVarValueInFile} from "../../../building-blocks/stages/writing/pieces/variables/replaceEnvVarValue";
+import {replaceStringValueInFile} from "../../../building-blocks/stages/writing/pieces/variables/replaceStringValue";
 
 export async function write_Auth_OAuth2_Keycloak_Stateful(
   _templateModel: EmptyTemplateModel, gen: YeomanGenerator
