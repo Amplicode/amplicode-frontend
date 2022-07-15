5f0b83e1a1ff875acd3d42e2327bc01d3a3b896a
Vyacheslav Pushkin <v.pushkin@haulmont.com>
2022-07-15 18:53:31 +0400

    WIP: keycloak jwt, pt.3

--- example-app/src/core/security/Auth.tsx
+++ example-app/src/core/security/Auth.tsx
@@ -1,36 +1,37 @@
 import { Result } from "antd";
 import {PropsWithChildren, useEffect, useState} from "react";
-import { useIntl } from "react-intl";
+import {FormattedMessage, useIntl} from "react-intl";
 import { useAuth } from "react-oidc-context";
 import {LoadingOutlined, LoginOutlined, LogoutOutlined} from "@ant-design/icons";
 import {ID_TOKEN_STORAGE_KEY} from "./oidcConfig";
 
 export function Auth({children}: PropsWithChildren<any>) {
   const auth = useAuth();
-  const intl = useIntl();
 
-  const [loggingIn, setLoggingIn] = useState<boolean>(false);
+  const [init, setInit] = useState<boolean>(false);
+
+  const {isLoading, activeNavigator, isAuthenticated, error, signinRedirect} = auth;
 
   useEffect(() => {
-    const {isLoading, activeNavigator, isAuthenticated, error} = auth;
-    if (!loggingIn && !isAuthenticated && !isLoading && !activeNavigator && !error) {
-      setLoggingIn(true);
-      auth.signinRedirect().then(() => {
-        setLoggingIn(false);
-      })
+    console.log('*************');
+
+    if (!init && !isAuthenticated && !isLoading && !activeNavigator && !error) {
+      console.log('REDIRECT SIGN IN');
+      setInit(true);
+      void signinRedirect();
     }
-  }, [auth, loggingIn]);
+  }, [activeNavigator, error, init, isAuthenticated, isLoading, signinRedirect]);
 
   switch (auth.activeNavigator) {
     case 'signinSilent':
       return (
-        <Result title={intl.formatMessage({id: 'auth.signingIn'})}
+        <Result title={<FormattedMessage id='auth.signingIn' />}
                 icon={<LoginOutlined/>}
         />
       );
     case 'signoutRedirect':
       return (
-        <Result title={intl.formatMessage({id: 'auth.signingOut'})}
+        <Result title={<FormattedMessage id='auth.signingOut' />}
                 icon={<LogoutOutlined/>}
         />
       );
@@ -42,8 +43,8 @@ export function Auth({children}: PropsWithChildren<any>) {
 
   if (auth.error) {
     return (
-      <Result title={intl.formatMessage({id: 'auth.failed'})}
-              subTitle={intl.formatMessage({id: 'common.unknownAppError'})}
+      <Result title={<FormattedMessage id='auth.failed' />}
+              subTitle={<FormattedMessage id='common.unknownAppError' />}
               status={'error'}
       />
     )
@@ -65,7 +66,7 @@ function Loading() {
   const intl = useIntl();
 
   return (
-    <Result title={intl.formatMessage({id: 'auth.loading'})}
+    <Result title={<FormattedMessage id='auth.loading' />}
             icon={<LoadingOutlined/>}
     />
   );
--- example-app/src/core/security/oidcConfig.ts
+++ example-app/src/core/security/oidcConfig.ts
@@ -1,20 +1,46 @@
 import { User } from "oidc-client-ts";
+import {resolvePath } from "react-router-dom";
 
 export const oidcConfig = {
   authority: 'https://keycloak.demo.haulmont.com/auth/realms/amplicode',
   client_id: 'petclinic',
   realm: 'amplicode',
-  redirect_uri: window.location.href,
+  redirect_uri: getRedirectUri(),
   scope: 'openid profile',
   onSigninCallback: async (user: void | User) => {
-    console.log('user', user);
-    if (user?.id_token != null) {
+    if (user != null && user?.id_token != null) {
       await localStorage.setItem(ID_TOKEN_STORAGE_KEY, user.id_token);
+      const redirectUri = (new URLSearchParams(window.location.search)).get('redirect_uri');
+      if (redirectUri != null) {
+        window.location.href = redirectUri;
+      } else {
+        console.error('redirect_uri not found');
+      }
     } else {
       console.error('id_token not found');
     }
-    window.history.replaceState({}, document.title, window.location.pathname);
   }
 };
 
 export const ID_TOKEN_STORAGE_KEY = `oidc.user:${oidcConfig.authority}:${oidcConfig.client_id}`;
+
+/**
+ * After login, authority will redirect the user to:
+ *
+ * http://localhost:3000/auth?redirect_uri={original_uri}&state=...&session_state=...&code=...
+ *
+ * where `state`, `session_state` and `code` are parameters added by authority,
+ * and `original_uri` is the location from where the user was redirected to authority login page
+ * (including query parameters).
+ *
+ * The reason we don't tell the authority to redirect directly to `original_uri` is to support
+ * the corner case of `original_uri` containing query parameters called `state`, `session_state` or `code`.
+ *
+ * The application doesn't have to actually contain the route /auth.
+ */
+function getRedirectUri() {
+  // Ensure correct path when app is deployed on non-root path
+  const authPath = resolvePath('auth').pathname;
+  const returnUrl = window.location.href;
+  return `${window.location.origin}${authPath}?redirect_uri=${returnUrl}`;
+}
